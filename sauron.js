d4a33df00866823b8da8913def8aabd79b8973dd
Trenton Menius <trenton296@goutlook.com>
2022-05-25 14:36:47 -0400

    Phase 2

--- sauron.js
+++ sauron.js
@@ -1,8 +1,23 @@
-let JonSnowAttack = 25;
-let JamieLannisterAttack = 35;
+//  Legolas Stats
+let LegolasAttack = 25;
+let LegolasHealth = 100;
+let LegolasDefense = 0;
 
-if (JonSnowAttack > JamieLannisterAttack) {
-  console.log("John Snow has better attack than Jamie Lannister");
+//gimli stats
+
+let GimliAttack = 35;
+
+if (LegolasAttack > GimliAttack) {
+  console.log("You shall die before your sword falls");
+} else if (LegolasAttack < GimliAttack) {
+  console.log("All die as equals");
 } else {
-  console.log("Jamie Lannister has better attack than Jon Snow");
+  console.log("We dwarves are deadly over short distances");
+}
+
+if (LegolasHealth <= GimliAttack) {
+  console.log("Legolas Dies");
+} else {
+  LegolasHealth -= GimliAttack;
+  console.log(`Legolas' Health is down to ${LegolasHealth}`);
 }
