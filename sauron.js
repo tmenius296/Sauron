2092cd065d1a3454e6182c7be984fae569a3eea4
Trenton Menius <trenton296@goutlook.com>
2022-05-25 14:16:46 -0400

    Initial Commit Middle Earth

---
+++ sauron.js
@@ -0,0 +1,8 @@
+let JonSnowAttack = 25;
+let JamieLannisterAttack = 35;
+
+if (JonSnowAttack > JamieLannisterAttack) {
+  console.log("John Snow has better attack than Jamie Lannister");
+} else {
+  console.log("Jamie Lannister has better attack than Jon Snow");
+}
